989b4cda0022f2c184f55858c170033217a74112
guzhen <effyroth@gmail.com>
2015-10-27 01:02:52 +0800

    add dubbo protocol rpc demo

--- src/main/java/com/effyroth/service/services/impl/TestServiceImpl.java
+++ src/main/java/com/effyroth/service/services/impl/TestServiceImpl.java
@@ -1,31 +1,29 @@
 package com.effyroth.service.services.impl;
 
-import com.effyroth.core.exceptions.TestException;
 import com.effyroth.core.services.TestService;
 import com.effyroth.core.structs.TestObject;
-import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by guzhen on 15/10/24.
  */
-public class TestServiceImpl implements TestService.Iface {
+public class TestServiceImpl implements TestService {
 
     Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
 
-    public TestObject testMethod(long testLong, String testString, boolean testBool) throws TestException, TException {
+    public TestObject testMethod(long testLong, String testString, boolean testBool){
         log.debug(String.valueOf(testLong));
         log.debug(testString);
         log.debug(String.valueOf(testBool));
         TestObject result = new TestObject(testString);
-        result.testBool = !testBool;
-        result.testLong = -testLong;
+        result.setTestBool(!testBool);
+        result.setTestLong(-testLong);
         return result;
     }
 
     @Override
-    public String testPing() throws TException {
+    public String testPing(){
         log.info("ping");
         return "pong";
     }
